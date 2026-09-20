b5464d5fd40157fa73296af7f94bc6eb55711e99
Diego Maldonado <dj_maldonado19@hotmail.es>
2026-09-20 12:29:45 -0600

    Feat: adding project files

---
+++ src/main/java/com/dmaldonado/contacto_3xtrat3rr3str3d/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.dmaldonado.contacto_3xtrat3rr3str3d;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.dmaldonado.contacto_3xtrat3rr3str3d {
+    requires javafx.controls;
+    exports com.dmaldonado.contacto_3xtrat3rr3str3d;
+}
